313babe0d661149a9c7f493ecd979f2dcc0ec921
Ging Marco Luca <marcoluca.ging@students.bfh.ch>
2024-05-12 11:54:59 +0200

    program working

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello world")
+#!/bin/python
+import sys
+
+BOOK_PATH="books/frankenstein.txt"
+
+def main():
+    with open(BOOK_PATH) as f:
+        file_contents = f.read()
+        print(file_contents)
+
+if __name__ == '__main__':
+    sys.exit(main())
