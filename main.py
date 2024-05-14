7c6d3c4a8704af5d55da08a8040a4c2067c23548
Ging Marco Luca <marcoluca.ging@students.bfh.ch>
2024-05-14 10:44:53 +0200

    Added word count

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 #!/bin/python
+
 import sys
 
 def main():
     book_path="books/frankenstein.txt"
     text=get_book_text(book_path)
-    print(text)
+    wc=get_word_count(text)
+    print(wc)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def get_word_count(text):
+    words = text.split()
+    return len(words)
+
+
 
 
 if __name__ == '__main__':
