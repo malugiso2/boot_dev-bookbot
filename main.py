f64b0b05042d7d50b1611d972072a24f9ff26cb3
Ging Marco Luca <marcoluca.ging@students.bfh.ch>
2024-05-14 10:57:53 +0200

    adding letter count

--- main.py
+++ main.py
@@ -5,18 +5,38 @@ import sys
 def main():
     book_path="books/frankenstein.txt"
     text=get_book_text(book_path)
+
     wc=get_word_count(text)
-    print(wc)
+    print(f"Number of words {wc}")
+
+    letter_occurance=get_letter_occurance(text)
+    print(f"letter dict {letter_occurance}")
+    # for key in letter_occurance:
+    #     print(f"{key}: {letter_occurance[key]}")
+
+
+
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
 
 def get_word_count(text):
     words = text.split()
     return len(words)
 
 
+def get_letter_occurance(text):
+    result = {}
+
+    for c in text.lower():
+        if c in result:
+            result[c] += 1
+        else:
+            result[c] = 1
+
+    return result
 
 
 if __name__ == '__main__':
